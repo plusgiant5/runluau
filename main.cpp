35e6f5833de3ce098c9fb3568340b7daaaf713f3
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-11-12 16:25:46 -0600

    chore(main.cpp): define ERROR_NOT_FOUND, ERROR_OUTOFMEMORY

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #ifndef _WIN32
     #define ERROR_INVALID_PARAMETER EINVAL
 	#define ERROR_INTERNAL_ERROR EIO
+	#define ERROR_NOT_FOUND ENOENT
+	#define ERROR_OUTOFMEMORY ENOMEM
 #endif
 
 #include <cerrno>
