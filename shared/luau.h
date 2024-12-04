25307b33423bc64f5c10469eedea105d9ee315fb
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-04 17:54:19 -0600

    chore: move macros into macros.h

--- shared/luau.h
+++ shared/luau.h
@@ -25,23 +25,7 @@ namespace fs = std::filesystem;
 #pragma pop_macro("max")
 #include <Luau/Compiler.h>
 #include "../Luau/VM/include/lualib.h"
-
-#ifdef PROJECT_EXPORTS
-// https://stackoverflow.com/a/78330763
-#if defined(_MSC_VER) && !defined(__clang__)
-#define API __declspec(dllexport)
-#else
-#error "TODO: Add support for non-MSVC compilers with PROJECT_EXPORTS"
-#endif
-#else
-// https://stackoverflow.com/a/78330763
-#if defined(_MSC_VER) && !defined(__clang__)
-#define API __declspec(dllimport)
-#else
-#define API __attribute__((visibility("default")))
-#define __fastcall __attribute__((fastcall))
-#endif
-#endif
+#include "macros.h"
 
 namespace luau {
 	API extern std::recursive_mutex luau_operation_mutex;
